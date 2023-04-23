87025dfb099f8f07b7111f17cb06349048c15efb
Valerii Chystiakov <hnu.kn20.2.chistyakov@gmail.com>
2023-04-23 15:27:28 +0300

    adding-mui-and-some-components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import Demo from './Demo';
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import Typography from '@mui/material/Typography'
+import Button from '@mui/material/Button'
+import IconButton from '@mui/material/IconButton'
+import MenuItem from '@mui/material/MenuItem'
+import MenuIcon from '@mui/icons-material/Menu';
+import Box from '@mui/material/Box';
+
 
 function App() {
+ 
+  
   return (
+    
     <div className="App">
+
       <header className="App-header">
+        <Demo/>
+        <AppBar position="sticky" color="default">
+          <Toolbar>
+              <IconButton aria-label="hello" >
+                <MenuItem ></MenuItem>
+              </IconButton>
+            <Typography variant="h6">
+              Hello
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +43,26 @@ function App() {
         >
           Learn React
         </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <Box sx={{ flexGrow: 1, width: '100%' }}>
+      <AppBar position="static">
+        <Toolbar sx={{display: 'flex',justifyContent: 'space-between'}}>
+          <Box sx={{display: 'flex', alignItems: "center"}}><IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            News
+          </Typography></Box>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
       </header>
     </div>
   );
---
+++ src/Ccomp.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+export default class Ccomp extends Component {
+    constructor(props) {
+        super(props)
+      
+        this.state = {
+           first
+        }
+      }
+      
+    render() {
+        
+      
+    return (
+      <div>Ccomp</div>
+    )
+  }
+}
